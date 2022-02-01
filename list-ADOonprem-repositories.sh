2ea532ce736a1f56eee2d2849a6b392e7f6d3510
Vani Sidagam <vanisdgm@gmail.com>
2022-02-01 15:10:33 +0530

    Update list-ADOonprem-repositories.sh

--- list-ADOonprem-repositories.sh
+++ list-ADOonprem-repositories.sh
@@ -2,7 +2,7 @@
 #read -p 'Bitbucket Username (not email): ' BB_USERNAME
 #read -sp 'Bitbucket Password: ' BB_PASSWORD
 #https://dev.azure.com/{organization}/_apis/git/repositories?api-version=6.0
-next_url="https://dev.azure.com/{organization}/_apis/git/repositories?api-version=6.0"
+next_url="https://dev.azure.com/vijeths01/_apis/git/repositories?api-version=6.0"
 while [ ! -z "$next_url" ]; do
  #   response_json=$( curl -s --user $ADO_USERNAME:$ADO_PASSWORD "$next_url" )
   #  echo "$response_json" | jq -r '.values | map([.slug, .workspace.slug, .description, .is_private] | @csv) | join("\n")'
