d65c4d2e5049e37ccf5303beb377d42c01fbfcde
vijeths <82367045+vijeths@users.noreply.github.com>
2022-02-08 11:48:11 +0530

    Update migrate-repositories-to-github.sh

--- migrate-repositories-to-github.sh
+++ migrate-repositories-to-github.sh
@@ -25,16 +25,16 @@ do
     echo "PUBLIC!"
     PRIVATE_FLAG="false"
   fi
-  curl -u vijeths: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 https://api.github.com/orgs/$gh_org/repos -d "{\"name\": \"$gh_repo\", \"private\": $PRIVATE_FLAG, \"description\": \"$description\"}"
+  curl -u vijeths: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 https://api.github.com/orgs/$gh_org/repos -d "{\"name\": \"$gh_repo\", \"private\": $PRIVATE_FLAG, \"description\": \"$description\"}"
   echo
 
   echo "=== pushing $gh_org/$gh_repo to GitHub..."
-  git push --mirror https://vijeths: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@github.com/$gh_org/$gh_repo.git
+  git push --mirror https://vijeths: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@github.com/$gh_org/$gh_repo.git
   echo
 
   if [ "$archive" = "yes" ]; then
     echo "=== archiving repository $gh_org/$gh_repo on GitHub..."
-    curl -X PATCH -u vijeths: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 https://api.github.com/repos/$gh_org/$gh_repo -d "{\"archived\": true}"
+    curl -X PATCH -u vijeths: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 https://api.github.com/repos/$gh_org/$gh_repo -d "{\"archived\": true}"
   fi
 
   cd ..  
