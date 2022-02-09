667391d3c29856fc309cce75cc9a2012f54ac605
vijeths <82367045+vijeths@users.noreply.github.com>
2022-02-09 12:13:52 +0530

    Update migrate-repositories-to-github.sh

--- migrate-repositories-to-github.sh
+++ migrate-repositories-to-github.sh
@@ -17,10 +17,8 @@
   
   echo
   
-  echo "###### Processing $ado_repo -> $gh_org/$gh_repo. Cloning from Bitbucket..."
-  #next_url="https://api.bitbucket.org/2.0/repositories?role=member"
-  #test=$(curl -s --user $ADO_USERNAME:$ADO_PASSWORD "$next_url")
-  #echo "$test"
+  echo "###### Processing $ado_repo -> $gh_org/$gh_repo. Cloning from ADO..."
+
   git clone --mirror https://vijeth565:kvaac3kw36q4hzzueqgsunzywl4yb6d4ifnwr7eozpkqu7mb5pva@dev.azure.com/vijeth565/Infy/_git/repoA
   cd $ado_repo.git
   echo
@@ -34,7 +32,7 @@
   #curl -u vijeths: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 https://api.github.com/orgs/$gh_org/repos -d "{\"name\": \"$gh_repo\", \"private\": $PRIVATE_FLAG, \"description\": \"$description\"}"
   #curl -u ${{ secrets.GITHUB_TOKEN }}:'https://api.github.com/orgs/$gh_org/repos -d "{\"name\": \"$gh_repo\", \"private\": $PRIVATE_FLAG, \"description\": \"$description\"}"'
   #curl -H 'Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}' --location --request 'https://api.github.com/orgs/$gh_org/repos'
-  curl -u ${{ secrets.GITHUB_TOKEN }} : "https://vijeths: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@github.com/vijeths/ghA.git"
+  #curl -u ${{ secrets.GITHUB_TOKEN }} : "https://vijeths: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@github.com/vijeths/ghA.git"
   #curl -u vijeths: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 --location --request GET 'https://api.github.com/orgs/$gh_org/repos -d "{\"name\": \"$gh_repo\", \"private\": $PRIVATE_FLAG, \"description\": \"$description\"}"'
   echo
 
