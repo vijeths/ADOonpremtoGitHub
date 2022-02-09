e25c94fe554ef55669719bd3f1146ba109720a3e
vijeths <82367045+vijeths@users.noreply.github.com>
2022-02-09 12:18:46 +0530

    Update migrate-repositories-to-github.sh

--- migrate-repositories-to-github.sh
+++ migrate-repositories-to-github.sh
@@ -37,7 +37,7 @@
   echo
 
   echo "=== pushing $gh_org/$gh_repo to GitHub..."
-  git push --mirror https://vijeths:${{ secrets.GITHUB_TOKEN }}@github.com/vijeths/ghA.git
+  git push --mirror https://vijeths:tn+*VjZWM23@github.com/vijeths/ghA.git
   echo
 
   #if [ "$archive" = "yes" ]; then
