fa3308f993a7b4bd3184b4f4c6a6a1b81d4e231f
vijeths <82367045+vijeths@users.noreply.github.com>
2022-02-09 11:45:07 +0530

    Update migrate-repositories-to-github.sh

--- migrate-repositories-to-github.sh
+++ migrate-repositories-to-github.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-if [[ ! -f $1 ]]; then echo "$1 file not found"; exit 99; fi
+#if [[ ! -f $1 ]]; then echo "$1 file not found"; exit 99; fi
 
 #read -p 'GitHub Username (not email): ' GH_USERNAME
 #read -sp 'GitHub Password: ' GH_PASSWORD
 #read -p 'BitBucket Username (not email): ' ADO_USERNAME
 #read -sp 'BitBucket Password: ' ADO_PASSWORD
 
-(cat "$1" ; echo) | tail -n +2 | tr -d '\r' | while IFS=, read -r ado_repo ado_org gh_repo gh_org description archive private
-do
-  if [ -z "$ado_repo" ]; then continue; fi # skip empty lines
+#(cat "$1" ; echo) | tail -n +2 | tr -d '\r' | while IFS=, read -r ado_repo ado_org gh_repo gh_org description archive private
+#do
+  #if [ -z "$ado_repo" ]; then continue; fi # skip empty lines
+  
+  $ado_repo = 'repoA'
+  $ado_org = 'vijeth565'
+  $gh_repo = 'ghA'
+  $gh_org = 'vijeths'
+  
   echo
   
   echo "###### Processing $ado_repo -> $gh_org/$gh_repo. Cloning from Bitbucket..."
@@ -28,18 +34,18 @@ do
   #curl -u vijeths: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 https://api.github.com/orgs/$gh_org/repos -d "{\"name\": \"$gh_repo\", \"private\": $PRIVATE_FLAG, \"description\": \"$description\"}"
   #curl -u ${{ secrets.GITHUB_TOKEN }}:'https://api.github.com/orgs/$gh_org/repos -d "{\"name\": \"$gh_repo\", \"private\": $PRIVATE_FLAG, \"description\": \"$description\"}"'
   #curl -H 'Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}' --location --request 'https://api.github.com/orgs/$gh_org/repos'
-  #curl -u ${{ secrets.GITHUB_TOKEN }} :"https://api.github.com/orgs/$gh_org/repos -d "{\"name\": \"$gh_repo\", \"private\": $PRIVATE_FLAG, \"description\": \"$description\"}""
-  curl -u vijeths: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 --location --request GET 'https://api.github.com/orgs/$gh_org/repos -d "{\"name\": \"$gh_repo\", \"private\": $PRIVATE_FLAG, \"description\": \"$description\"}"'
+  curl -u ${{ secrets.GITHUB_TOKEN }} : "https://vijeths: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@github.com/$gh_org/$gh_repo.git"
+  #curl -u vijeths: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 --location --request GET 'https://api.github.com/orgs/$gh_org/repos -d "{\"name\": \"$gh_repo\", \"private\": $PRIVATE_FLAG, \"description\": \"$description\"}"'
   echo
 
   echo "=== pushing $gh_org/$gh_repo to GitHub..."
-  git push --mirror https://vijeths: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@github.com/$gh_org/$gh_repo.git
+  git push --mirror https://vijeths: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@github.com/$gh_org/$gh_repo.git
   echo
 
-  if [ "$archive" = "yes" ]; then
-    echo "=== archiving repository $gh_org/$gh_repo on GitHub..."
-    curl -X PATCH -u vijeths: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 https://api.github.com/repos/$gh_org/$gh_repo -d "{\"archived\": true}"
-  fi
+  #if [ "$archive" = "yes" ]; then
+   # echo "=== archiving repository $gh_org/$gh_repo on GitHub..."
+    #curl -X PATCH -u vijeths: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 https://api.github.com/repos/$gh_org/$gh_repo -d "{\"archived\": true}"
+  #fi
 
   cd ..  
-done
+#done
