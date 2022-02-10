8e18af6702550052b511b340eaf3ef1e03dbefca
vijeths <82367045+vijeths@users.noreply.github.com>
2022-02-10 11:25:34 +0530

    Create migrate.sh

---
+++ migrate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ git clone --mirror https://vijeth565:kvaac3kw36q4hzzueqgsunzywl4yb6d4ifnwr7eozpkqu7mb5pva@dev.azure.com/vijeth565/Infy/_git/repoA
+ cd repoA
+ pwd 
+ ls
+ git push --mirror https://vijeths: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@github.com/vijeths/ghA.git
+ 
